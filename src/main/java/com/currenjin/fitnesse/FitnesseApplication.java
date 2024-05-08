be6d80b310bc764af07281a48595fb1a6469fe5b
currenjin <hyun0524e@naver.com>
2024-05-08 20:36:21 +0900

    Init: Project

---
+++ src/main/java/com/currenjin/fitnesse/FitnesseApplication.java
@@ -0,0 +1,13 @@
+package com.currenjin.fitnesse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnesseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnesseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/currenjin/fitnesse/FitnesseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currenjin.fitnesse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
